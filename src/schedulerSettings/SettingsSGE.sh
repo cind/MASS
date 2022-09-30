29694d2ed5c4b3d526b2c4e05957b6aef11a53a8
toomanycats <daniel.cuneo@ncire.org>
2022-09-29 17:04:15 -0700

    set queue name

--- src/schedulerSettings/SettingsSGE.sh
+++ src/schedulerSettings/SettingsSGE.sh
@@ -2,11 +2,11 @@
 
 setSubmitCMD()
 {
-	# Getting the arguments 
+	# Getting the arguments
 	_memory=$1
 	shift
 	_holdids="$*"
-	
+
 	# Getting the options for SGE scheduler into variables
 	# This is done so that the user can adapt these options to match them with his own scheduler
 	_OptExec="qsub"							# Executable name for job scheduler
@@ -16,22 +16,23 @@ setSubmitCMD()
 	_OptJoin="-j y"							# String for joining the output files
 	_OptOutput="-o ${log}\$JOB_NAME-\$JOB_ID.log"			# String for specifying output filename
 	_OptHoldID="-hold_jid"						# String for requesting a job dependency
-	
-	
+    _Queue="-q low.q"
+
+
 	# Setting up the basic command and then the other options will be added onto it
 	submitCMD="${_OptExec} ${_OptTerse} ${_OptJoin} ${_OptOutput} ${_OptMemory}"
-	
-	
+
+
 	# check if priority variable
 	if [ -n "${priority}" ]
-	then 
+	then
 		submitCMD="${submitCMD} ${_OptPriority} ${priority}";
 	fi
 
 	# check if the user provided a hold jobid
 	# Modify this part of the code depending on how the job ids need to be appended
 	if [ -n "${_holdids}" ]
-	then 
+	then
 		for _id in ${_holdids}
 		do
 			submitCMD="${submitCMD} ${_OptHoldID} ${_id}"
@@ -45,10 +46,10 @@ setDeleteJob()
 {
 	# Getting the job IDs
 	_jobid="$*"
-	
+
 	# Getting the options for SGE scheduler into variables
 	# This is done so that the user can adapt these options to match them with his own scheduler
 	_OptJobDeleteExec="qdel"					# Executable name for deleting jobs
-	
+
 	echo ${_OptJobDeleteExec}
 }
